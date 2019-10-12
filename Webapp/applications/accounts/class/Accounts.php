9a37a78db2a17c6b67a6d5b7333fe8026d0da04c
durianpeople <durianpeople@gmail.com>
2019-10-12 13:41:07 +0700

    remove webapp

--- Webapp/applications/accounts/class/Accounts.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Accounts
-{
-    public function __construct()
-    {
-        throw new PuzzleError("Class Accounts has been deprecated in 3.1. Please use PuzzleUser!");
-    }
-}
--- Webapp/templates/admin_theme/404.php
+++
@@ -1,7 +0,0 @@
-<div style="
-    width: 100%;
-    height: 80%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 100pt;">404</div>
